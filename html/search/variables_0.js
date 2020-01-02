fb64ce1002a30edb72805bf24684addba229be36
Havea7 <w63477@student.wsiz.rzeszow.pl>
2020-01-02 03:04:32 +0100

    commit all doxygen

---
+++ html/search/variables_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['enumptr_17',['enumPtr',['../class_q_tstyle___test.html#a973a4566c9a036f4eca508ba5fe80dcb',1,'QTstyle_Test']]],
+  ['enumvar_18',['enumVar',['../class_q_tstyle___test.html#a241fb54f66dc0b3b03eece3a1a1bf18b',1,'QTstyle_Test']]]
+];
